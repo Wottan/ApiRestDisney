49ce49a87d82bcfdc225a7e6d504934fb17fbdbb
Ivan Nicolas <ivannsuchan@gmail.com>
2022-05-04 23:15:23 -0300

    First

---
+++ src/main/java/com/ApiRestDisney/personajes/ApiRestDisneyApplication.java
@@ -0,0 +1,13 @@
+package com.ApiRestDisney.personajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDisneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDisneyApplication.class, args);
+	}
+
+}
