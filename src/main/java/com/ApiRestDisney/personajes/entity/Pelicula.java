4a5edee8f5db976536ccd756cdc6ae3b5eaa8148
Ivan Nicolas <ivannsuchan@gmail.com>
2022-05-05 00:00:09 -0300

    Added attributes to Pelicula

--- src/main/java/com/ApiRestDisney/personajes/entity/Pelicula.java
+++ src/main/java/com/ApiRestDisney/personajes/entity/Pelicula.java
@@ -1,5 +1,41 @@
 package com.ApiRestDisney.personajes.entity;
 
-public class Pelicula {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "peliculas")
+public class Pelicula implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "nombre")
+	private String titulo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
 }
