c129335a3e0be749f78fd930bef240dd38b83551
Ivan Nicolas <ivannsuchan@gmail.com>
2022-05-06 10:59:50 -0300

    Added enum to pelicula

--- src/main/java/com/ApiRestDisney/personajes/entity/Pelicula.java
+++ src/main/java/com/ApiRestDisney/personajes/entity/Pelicula.java
@@ -1,15 +1,19 @@
 package com.ApiRestDisney.personajes.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-//dfgf
 @Entity
 @Table(name = "peliculas")
 public class Pelicula implements Serializable {
@@ -22,6 +26,21 @@ public class Pelicula implements Serializable {
 
 	@Column(name = "nombre")
 	private String titulo;
+
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+
+	@Enumerated(EnumType.STRING)
+	private Calificacion calificacion;
+
+	public Pelicula() {
+		super();
+	}
+
+	public enum Calificacion {
+		Uno, Dos, Tres, Cuatro, Cinco;
+	}
 
 	public Long getId() {
 		return id;
