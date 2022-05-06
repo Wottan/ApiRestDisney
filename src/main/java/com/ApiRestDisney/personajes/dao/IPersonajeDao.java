8e125276a425c483e75be0d40c69ca98cd04f52d
Ivan Nicolas <ivannsuchan@gmail.com>
2022-05-06 14:53:01 -0300

    Added IPersonajeDao

---
+++ src/main/java/com/ApiRestDisney/personajes/dao/IPersonajeDao.java
@@ -0,0 +1,17 @@
+package com.ApiRestDisney.personajes.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ApiRestDisney.personajes.entity.Personaje;
+
+public interface IPersonajeDao extends CrudRepository<Personaje, Long> {
+
+	public Personaje findByNombre(String nombre);
+
+	public Personaje findByPeso(Double peso);
+
+	public Personaje findByEdad(String edad);
+
+//	public Personaje findByPeliculas(Long idMovie);
+
+}
